dbd6982dab205987c6f8c36515ffc323fcd0812f
EXTENTIA05\Sachin.Ranvirkar <sachin.ranvirkar@extentia.com>
2017-09-16 04:59:30 +0530

    asasd
    asdasd

--- serverApp/model/userModel.js
+++ serverApp/model/userModel.js
@@ -27,8 +27,8 @@ UserModel.prototype.createUser = function(record){
 					reject(that.serverResponse);
 				}else{
 					console.log(record);
-					var queryStr = "INSERT INTO public.tfpuser(fname, lname, email, user_name, pass, phone_number, profile_pic, background_check, active)VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9)";
-					var query =  client.query(queryStr, [record.fname, record.lname, record.email, record.user_name, record.pass, record.phone_number, record.profile_pic, record.background_check, record.active]);
+					var queryStr = "INSERT INTO public.tfpuser(name, email, user_name, pass, phone_number, profile_pic, background_check, active)VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9)";
+					var query =  client.query(queryStr, [record.name, record.email, record.email, record.pass, record.phone_number, record.profile_pic, record.background_check, record.active]);
 					query.on('end', function(err, res) {
 						console.log(err, res);
 						done();				
