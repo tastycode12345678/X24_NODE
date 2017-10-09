387d77afac7d9d53c5d886c4192430c14baf6974
Sachin Ranvirkar <Sachin.Ranvirkar@extentia05.local>
2017-10-09 12:20:13 +0530

    base64 changes
    base64 changes

--- serverApp/routes/watsonplatformRoute.js
+++ serverApp/routes/watsonplatformRoute.js
@@ -16,20 +16,24 @@ module.exports = function(app) {
 		var bsString = req.body.imageFile;
 		if(bsString){
 			var matches = bsString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-			if(matches.length === 3){
-				
-				var baseUrl = req.protocol + '://' + req.get('host');
-				var imageType = matches[1].replace("image/","");
-				var fileDirectory = "./public/images/";
-				var filePathDirectory = "/images/";
-				var fileName = new Date().getTime() +"."+ imageType;
-				base64_decode(matches[2], fileDirectory+fileName);				
-				watsonplatformModel.getWatsonInfo(baseUrl+filePathDirectory+fileName).then(function(response){
-					res.send(response);
-				}, function(err){
-					res.status(400);
-					res.send(err);
-				});
+			if(matches){
+				if(matches.length === 3){
+					
+					var baseUrl = req.protocol + '://' + req.get('host');
+					var imageType = matches[1].replace("image/","");
+					var fileDirectory = "./public/images/";
+					var filePathDirectory = "/images/";
+					var fileName = new Date().getTime() +"."+ imageType;
+					base64_decode(matches[2], fileDirectory+fileName);				
+					watsonplatformModel.getWatsonInfo(baseUrl+filePathDirectory+fileName).then(function(response){
+						res.send(response);
+					}, function(err){
+						res.status(400);
+						res.send(err);
+					});
+				}else{
+					res.send("Invalid base64 input");
+				}
 			}else{
 				res.send("Invalid base64 input");
 			}
