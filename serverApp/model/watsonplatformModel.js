8147c82c22906977572785d9f3d0bc308be5cec2
Sachin Ranvirkar <Sachin.Ranvirkar@extentia05.local>
2017-10-09 12:15:32 +0530

    ibmwatson base64 image file changes
    ibmwatson base64 image file changes

--- serverApp/model/watsonplatformModel.js
+++ serverApp/model/watsonplatformModel.js
@@ -1,79 +1,30 @@
 var Promise = require('promise');
-var requestModule = require('request');
-var Base64Decode = require('base64-stream').decode;
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-//var base64Img = require('base64-img');
-var fs = require('fs');
 
 function watsonplatformModel(){
 
 }
-watsonplatformModel.prototype.getWatsonInfo = function(userObject){
-	var serverResponse = {
-		'success': 0,
-		'error': 0,
-		'response': {}
-	}
-	//userObject.images_files["value"] = new Base64Decode(userObject.images_files["value"]);
+watsonplatformModel.prototype.getWatsonInfo = function(imageUrl){
 	console.log("just for showing demo.......");
+	var visual_recognition = new VisualRecognitionV3({
+		api_key: '94190f7444d1dbabb97a70c0315d0223cb058edd',
+  		version_date: "2016-05-20"
+	});
+	var params = {
+		url: imageUrl,
+		classifier_ids: "TrustedFactorVRService_1054403198"
+	};
 	return new Promise(function(resolve, reject){
 		try{
-			var v3 = new VisualRecognitionV3({
-				api_key: '94190f7444d1dbabb97a70c0315d0223cb058edd',
-  				version_date: "2017-09-17"
-			});
-			v3.classify({
-				url: "https://visistors-x24.herokuapp.com/images/Naazneen.jpg",
-				classifier_ids: "TrustedFactorVRService_1054403198"
-			}, function(err, result){
+			visual_recognition.classify(params, function(err, result){
 				if (err){
-					serverResponse.error = 1;
-					serverResponse.response = err;
-					reject(serverResponse);
+					reject(err);
 				}else{	
-					console.log("result  ", result);
-					if(result.images){
-						if(result.images.length){
-							if(result.images[0].classifiers){
-								if(result.images[0].classifiers.length){
-									if(result.images[0].classifiers[0].classes){
-										serverResponse.success = 1;
-										serverResponse.response = result.images[0].classifiers[0].classes[0];
-										resolve(serverResponse);
-									}else{	
-										serverResponse.error = 1;
-										serverResponse.response = result.images[0].classifiers;
-										reject(serverResponse);
-									}								
-								}else{
-									serverResponse.error = 1;
-									serverResponse.response = null;
-									reject(serverResponse);
-								}
-							}else{
-								serverResponse.error = 1;
-								serverResponse.response = null;
-								reject(serverResponse);
-							}
-							serverResponse.success = 1;
-							serverResponse.response = result.images[0].classifiers[0].classes[0];
-							resolve(serverResponse);						
-						}else{
-							serverResponse.error = 1;
-							serverResponse.response = null;
-							reject(serverResponse);
-						}
-					}else{
-						serverResponse.error = 1;
-						serverResponse.response = null;
-						reject(serverResponse);
-					}			
+					resolve(JSON.stringify(result, null, 2));			
 				}
 			});			
 		} catch(e){
-			serverResponse.error = 1;
-			serverResponse.response = e;
-			reject(serverResponse);
+			reject(e);
 		}
 	});
 }
--- serverApp/routes/watsonplatformRoute.js
+++ serverApp/routes/watsonplatformRoute.js
@@ -1,14 +1,42 @@
 var watsonplatformModel = require('../model/watsonplatformModel').watsonplatformModel;
+var fs = require('fs');
 
 module.exports = function(app) {
-
-	app.post('/watsonplatform', function(req, res){
-		watsonplatformModel.getWatsonInfo(req.body).then(function(response){
-			res.send(response);
-		}, function(err){
-			res.status(400);
-			res.send(err);
-		});
+	
+	app.post('/watsonplatform', function(req, res){	
+	
+		// function to create file from base64 encoded string
+		var base64_decode = function(base64str, file) {
+			var bitmap = new Buffer(base64str, 'base64');
+			fs.writeFileSync(file, bitmap);
+			console.log('******** File created from base64 encoded string ********');
+		};
+	
+		
+		var bsString = req.body.imageFile;
+		if(bsString){
+			var matches = bsString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+			if(matches.length === 3){
+				
+				var baseUrl = req.protocol + '://' + req.get('host');
+				var imageType = matches[1].replace("image/","");
+				var fileDirectory = "./public/images/";
+				var filePathDirectory = "/images/";
+				var fileName = new Date().getTime() +"."+ imageType;
+				base64_decode(matches[2], fileDirectory+fileName);				
+				watsonplatformModel.getWatsonInfo(baseUrl+filePathDirectory+fileName).then(function(response){
+					res.send(response);
+				}, function(err){
+					res.status(400);
+					res.send(err);
+				});
+			}else{
+				res.send("Invalid base64 input");
+			}
+		}else{
+			res.send("Invalid input");
+		}
+		
 	});
 
 };
